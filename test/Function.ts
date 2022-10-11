63673275365b029684135958a58e69093cc49187
gcanti <giulio.canti@gmail.com>
2022-10-11 06:41:03 +0200

    boolean: renaming

--- test/Function.ts
+++ test/Function.ts
@@ -120,8 +120,8 @@ describe("Function", () => {
   })
 
   it("getMonoid", () => {
-    const getPredicateMonoidAll = _.getMonoid(B.MonoidAnd)
-    const getPredicateMonoidAny = _.getMonoid(B.MonoidOr)
+    const getPredicateMonoidAll = _.getMonoid(B.MonoidAll)
+    const getPredicateMonoidAny = _.getMonoid(B.MonoidAny)
 
     const isLessThan10 = (n: number) => n <= 10
     const isEven = (n: number) => n % 2 === 0
