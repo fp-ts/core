9a3a86295d3b3e2541a6813f7b26ff22665de4bf
gcanti <giulio.canti@gmail.com>
2022-10-11 14:43:31 +0200

    fix dtslint tests

--- dtslint/ts4.1/Semigroup.ts
+++ dtslint/ts4.1/Semigroup.ts
@@ -9,14 +9,14 @@ import * as B from '../../src/boolean'
 //
 
 // $ExpectType Semigroup<{ readonly a: string; readonly b: number; readonly c: boolean; }>
-_.struct({ a: S.Semigroup, b: N.SemigroupSum, c: B.SemigroupAnd })
+_.struct({ a: S.Semigroup, b: N.SemigroupSum, c: B.SemigroupAll })
 
 //
 // tuple
 //
 
 // $ExpectType Semigroup<readonly [string, number, boolean]>
-_.tuple(S.Semigroup, N.SemigroupSum, B.SemigroupAnd)
+_.tuple(S.Semigroup, N.SemigroupSum, B.SemigroupAll)
 
 //
 // concatAll
