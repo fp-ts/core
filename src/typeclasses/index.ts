56ff55bfa2bbc46a10c2cfff602fe64b14c88716
gcanti <giulio.canti@gmail.com>
2022-10-11 14:42:42 +0200

    typeclasses: add index

---
+++ src/typeclasses/index.ts
@@ -0,0 +1,221 @@
+/**
+ * @since 3.0.0
+ */
+import * as alt from "@fp-ts/core/typeclasses/Alt"
+import * as alternative from "@fp-ts/core/typeclasses/Alternative"
+import * as applicative from "@fp-ts/core/typeclasses/Applicative"
+import * as apply from "@fp-ts/core/typeclasses/Apply"
+import * as bifunctor from "@fp-ts/core/typeclasses/Bifunctor"
+import * as bounded from "@fp-ts/core/typeclasses/Bounded"
+import * as category from "@fp-ts/core/typeclasses/Category"
+import * as comonad from "@fp-ts/core/typeclasses/Comonad"
+import * as compactable from "@fp-ts/core/typeclasses/Compactable"
+import * as composable from "@fp-ts/core/typeclasses/Composable"
+import * as contravariant from "@fp-ts/core/typeclasses/Contravariant"
+import * as eq from "@fp-ts/core/typeclasses/Eq"
+import * as extendable from "@fp-ts/core/typeclasses/Extendable"
+import * as filterable from "@fp-ts/core/typeclasses/Filterable"
+import * as flattenable from "@fp-ts/core/typeclasses/Flattenable"
+import * as fromAsync from "@fp-ts/core/typeclasses/FromAsync"
+import * as fromIdentity from "@fp-ts/core/typeclasses/FromIdentity"
+import * as fromOption from "@fp-ts/core/typeclasses/FromOption"
+import * as fromReader from "@fp-ts/core/typeclasses/FromReader"
+import * as fromResult from "@fp-ts/core/typeclasses/FromResult"
+import * as fromState from "@fp-ts/core/typeclasses/FromState"
+import * as fromSync from "@fp-ts/core/typeclasses/FromSync"
+import * as fromThese from "@fp-ts/core/typeclasses/FromThese"
+import * as fromWriter from "@fp-ts/core/typeclasses/FromWriter"
+import * as functor from "@fp-ts/core/typeclasses/Functor"
+import * as invariant from "@fp-ts/core/typeclasses/Invariant"
+import * as kleisliCategory from "@fp-ts/core/typeclasses/KleisliCategory"
+import * as kleisliComposable from "@fp-ts/core/typeclasses/KleisliComposable"
+import * as monad from "@fp-ts/core/typeclasses/Monad"
+import * as monoid from "@fp-ts/core/typeclasses/Monoid"
+import * as ord from "@fp-ts/core/typeclasses/Ord"
+import * as ordering from "@fp-ts/core/typeclasses/Ordering"
+import * as semigroup from "@fp-ts/core/typeclasses/Semigroup"
+import * as show from "@fp-ts/core/typeclasses/Show"
+import * as traversable from "@fp-ts/core/typeclasses/Traversable"
+import * as traversableFilterable from "@fp-ts/core/typeclasses/TraversableFilterable"
+
+export {
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  alt,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  alternative,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  applicative,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  apply,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  bifunctor,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  bounded,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  category,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  comonad,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  compactable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  composable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  contravariant,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  eq,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  extendable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  filterable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  flattenable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromAsync,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromIdentity,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromOption,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromReader,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromResult,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromState,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromSync,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromThese,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  fromWriter,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  functor,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  invariant,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  kleisliCategory,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  kleisliComposable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  monad,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  monoid,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  ord,
+  /**
+   * @since 3.0.0
+   */
+  ordering,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  semigroup,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  show,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  traversable,
+  /**
+   * @category type classes
+   * @since 3.0.0
+   */
+  traversableFilterable
+}
