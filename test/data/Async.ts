e95afaa3cca8e11e35ad9014f94dad2d35a50ec2
gcanti <giulio.canti@gmail.com>
2022-10-13 18:45:28 +0200

    refactor FirstSuccessOf

--- test/data/Async.ts
+++ test/data/Async.ts
@@ -1,4 +1,3 @@
-import type * as firstSuccessOf_ from "@fp-ts/core/FirstSuccessOf"
 import type { TypeLambda } from "@fp-ts/core/HKT"
 import type { Result } from "./Result"
 
@@ -9,12 +8,3 @@ export interface Async<R, E, A> {
 export interface AsyncTypeLambda extends TypeLambda {
   readonly type: Async<this["In1"], this["Out2"], this["Out1"]>
 }
-
-export declare const FirstSuccessOf: firstSuccessOf_.FirstSuccessOf<AsyncTypeLambda>
-
-declare const a: Async<{ a: string }, "a", string>
-declare const b: Async<{ b: string }, "b", number>
-declare const c: Async<{ c: string }, "c", boolean>
-
-export const x = FirstSuccessOf.firstSuccessOf(a, b, c)
-export const y = FirstSuccessOf.firstSuccessOfIterable(a, [b, c])
