2b74cc108692904ae5e829db81a18afaacfd66ff
gcanti <giulio.canti@gmail.com>
2022-10-14 09:47:44 +0200

    chore

---
+++ dtslint/ts4.1/Failable.ts
@@ -0,0 +1,21 @@
+import type * as failable from "@fp-ts/core/Failable"
+import type { TypeLambda } from "@fp-ts/core/HKT"
+
+export interface ReaderAsyncWriter<R, E, A> {
+  (r: R): () => Promise<[E, A]>
+}
+
+export interface ReaderAsyncWriterTypeLambda extends TypeLambda {
+  readonly type: ReaderAsyncWriter<this["In1"], this["Out2"], this["Out1"]>
+}
+
+export declare const Failable: failable.Failable<ReaderAsyncWriterTypeLambda>
+
+declare const a: ReaderAsyncWriter<{ a: string }, "a", string>
+declare const b: ReaderAsyncWriter<{ b: string }, "b", number>
+declare const c: ReaderAsyncWriter<{ b: string }, "b", number>
+
+// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: string; }, "a" | "b", string | number>
+Failable.firstSuccessOf(a, b, c)
+// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: string; }, "a" | "b", string | number>
+Failable.firstSuccessOfAllWith(a, [b, c])
