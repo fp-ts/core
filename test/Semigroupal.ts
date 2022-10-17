db002155db73864df12ac0a88aaa172428807b7c
gcanti <giulio.canti@gmail.com>
2022-10-17 08:16:26 +0200

    more tests

--- test/Semigroupal.ts
+++ test/Semigroupal.ts
@@ -1,6 +1,7 @@
 import { pipe } from "@fp-ts/core/internal/Function"
 import * as _ from "@fp-ts/core/Semigroupal"
 import * as O from "./data/Option"
+import * as string from "./data/string"
 import * as U from "./util"
 
 describe("Semigroupal", () => {
@@ -41,6 +42,43 @@ describe("Semigroupal", () => {
     U.deepStrictEqual(pipe(O.none, zip(O.some(1))), O.none)
     U.deepStrictEqual(pipe(O.some(1), zip(O.none)), O.none)
     U.deepStrictEqual(pipe(O.some(1), zip(O.some("a"))), O.some([1, "a"] as const))
+  })
+
+  it("zipLeftPar", () => {
+    const zipLeftPar = _.zipLeftPar(O.Semigroupal)
+    U.deepStrictEqual(pipe(O.none, zipLeftPar(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.none, zipLeftPar(O.some(2))), O.none)
+    U.deepStrictEqual(pipe(O.some(1), zipLeftPar(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some(1), zipLeftPar(O.some(2))), O.some(1))
+  })
+
+  it("zipRightPar", () => {
+    const zipRightPar = _.zipRightPar(O.Semigroupal)
+    U.deepStrictEqual(pipe(O.none, zipRightPar(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.none, zipRightPar(O.some(2))), O.none)
+    U.deepStrictEqual(pipe(O.some(1), zipRightPar(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some(1), zipRightPar(O.some(2))), O.some(2))
+  })
+
+  it("bindRight", () => {
+    const bindRight = _.bindRight(O.Semigroupal)
+    U.deepStrictEqual(pipe(O.some({ a: 1 }), bindRight("b", O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some({ a: 1 }), bindRight("b", O.some(2))), O.some({ a: 1, b: 2 }))
+  })
+
+  it("zipFlatten", () => {
+    const zipFlatten = _.zipFlatten(O.Semigroupal)
+    U.deepStrictEqual(pipe(O.some([1, 2]), zipFlatten(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some([1, 2]), zipFlatten(O.some(3))), O.some([1, 2, 3] as const))
+  })
+
+  it("liftSemigroup", () => {
+    const liftSemigroup = _.liftSemigroup(O.Semigroupal)
+    const S = liftSemigroup(string.Semigroup)
+    U.deepStrictEqual(pipe(O.none, S.combine(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.none, S.combine(O.some("b"))), O.none)
+    U.deepStrictEqual(pipe(O.some("a"), S.combine(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some("a"), S.combine(O.some("b"))), O.some("ab"))
   })
 
   it("lift2", () => {
