8883b74d5df74cda5ee5aefb68d83369b881bab2
gcanti <giulio.canti@gmail.com>
2022-10-19 16:13:16 +0200

    chore

--- src/typeclass/Bicovariant.ts
+++ src/typeclass/Bicovariant.ts
@@ -10,10 +10,10 @@ import type { Covariant } from "@fp-ts/core/typeclass/Covariant"
  * @since 1.0.0
  */
 export interface Bicovariant<F extends TypeLambda> extends TypeClass<F> {
-  readonly bimap: <E1, E2, A1, A2>(
+  readonly bimap: <E1, E2, A, B>(
     f: (e: E1) => E2,
-    g: (a: A1) => A2
-  ) => <S, R, O>(self: Kind<F, S, R, O, E1, A1>) => Kind<F, S, R, O, E2, A2>
+    g: (a: A) => B
+  ) => <S, R, O>(self: Kind<F, S, R, O, E1, A>) => Kind<F, S, R, O, E2, B>
 }
 
 /**
@@ -33,9 +33,9 @@ export const mapLeft = <F extends TypeLambda>(
  * @since 1.0.0
  */
 export const map = <F extends TypeLambda>(Bicovariant: Bicovariant<F>): Covariant<F>["map"] =>
-  <A1, A2>(
-    f: (a: A1) => A2
-  ): (<S, R, O, E>(self: Kind<F, S, R, O, E, A1>) => Kind<F, S, R, O, E, A2>) =>
+  <A, B>(
+    f: (a: A) => B
+  ): (<S, R, O, E>(self: Kind<F, S, R, O, E, A>) => Kind<F, S, R, O, E, B>) =>
     Bicovariant.bimap(identity, f)
 
 /**
@@ -47,10 +47,10 @@ export const bimapComposition = <F extends TypeLambda, G extends TypeLambda>(
   CovariantF: Covariant<F>,
   BicovariantG: Bicovariant<G>
 ) =>
-  <GE1, GE2, A1, A2>(
+  <GE1, GE2, A, B>(
     f: (e: GE1) => GE2,
-    g: (a: A1) => A2
+    g: (a: A) => B
   ): (<FS, FR, FO, FE, GS, GR, GO>(
-    self: Kind<F, FS, FR, FO, FE, Kind<G, GS, GR, GO, GE1, A1>>
-  ) => Kind<F, FS, FR, FO, FE, Kind<G, GS, GR, GO, GE2, A2>>) =>
+    self: Kind<F, FS, FR, FO, FE, Kind<G, GS, GR, GO, GE1, A>>
+  ) => Kind<F, FS, FR, FO, FE, Kind<G, GS, GR, GO, GE2, B>>) =>
     CovariantF.map(BicovariantG.bimap(f, g))
