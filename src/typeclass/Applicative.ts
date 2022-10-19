4ba085881501c1ae3a6afa1a4b45b2bbed872bed
gcanti <giulio.canti@gmail.com>
2022-10-19 16:37:10 +0200

    Applicative: replace unit with of

--- src/typeclass/Applicative.ts
+++ src/typeclass/Applicative.ts
@@ -2,9 +2,9 @@
  * @since 1.0.0
  */
 import type { Kind, TypeLambda } from "@fp-ts/core/HKT"
-import { pipe } from "@fp-ts/core/internal/Function"
 import type { Monoid } from "@fp-ts/core/typeclass/Monoid"
 import * as monoid from "@fp-ts/core/typeclass/Monoid"
+import type { Of } from "@fp-ts/core/typeclass/Of"
 import type { Product } from "@fp-ts/core/typeclass/Product"
 import * as product from "@fp-ts/core/typeclass/Product"
 
@@ -12,9 +12,7 @@ import * as product from "@fp-ts/core/typeclass/Product"
  * @category type class
  * @since 1.0.0
  */
-export interface Applicative<F extends TypeLambda> extends Product<F> {
-  readonly unit: <S>() => Kind<F, S, unknown, never, never, unknown>
-
+export interface Applicative<F extends TypeLambda> extends Product<F>, Of<F> {
   readonly productAll: <S, R, O, E, A>(
     collection: Iterable<Kind<F, S, R, O, E, A>>
   ) => Kind<F, S, R, O, E, ReadonlyArray<A>>
@@ -29,5 +27,5 @@ export const liftMonoid = <F extends TypeLambda>(Applicative: Applicative<F>) =>
   <A, S, R, O, E>(Monoid: Monoid<A>): Monoid<Kind<F, S, R, O, E, A>> =>
     monoid.fromAssociative(
       product.liftAssociative(Applicative)<A, S, R, O, E>(Monoid),
-      pipe(Applicative.unit<S>(), Applicative.map(() => Monoid.unit))
+      Applicative.of(Monoid.unit)
     )
