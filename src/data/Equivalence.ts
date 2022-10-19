5fe93fa1d6c403bccd4c6d66c6e958f797513a86
gcanti <giulio.canti@gmail.com>
2022-10-19 12:05:08 +0200

    move Equivalence constructor to internal

--- src/data/Equivalence.ts
+++ src/data/Equivalence.ts
@@ -10,12 +10,3 @@ export interface Equivalence<A, B> {
   readonly to: (a: A) => B
   readonly from: (b: B) => A
 }
-
-/**
- * @category constructors
- * @since 1.0.0
- */
-export const make = <A, B>(to: (a: A) => B, from: (b: B) => A): Equivalence<A, B> => ({
-  to,
-  from
-})
--- src/data/TotalOrdering.ts
+++ src/data/TotalOrdering.ts
@@ -1,57 +1,9 @@
 /**
  * @since 1.0.0
  */
-import type * as associative from "@fp-ts/core/typeclass/Associative"
-import * as monoid from "@fp-ts/core/typeclass/Monoid"
 
 /**
  * @category data
  * @since 1.0.0
  */
 export type TotalOrdering = -1 | 0 | 1
-
-/**
- * @since 1.0.0
- */
-export const reverse = (
-  totalOrdering: TotalOrdering
-): TotalOrdering => (totalOrdering === -1 ? 1 : totalOrdering === 1 ? -1 : 0)
-
-/**
- * @category pattern matching
- * @since 1.0.0
- */
-export const match = <A, B, C = B>(
-  onLessThan: () => A,
-  onEqual: () => B,
-  onGreaterThan: () => C
-) =>
-  (totalOrdering: TotalOrdering): A | B | C =>
-    totalOrdering === -1 ? onLessThan() : totalOrdering === 0 ? onEqual() : onGreaterThan()
-
-/**
- * @category instances
- * @since 1.0.0
- */
-export const Associative: associative.Associative<TotalOrdering> = {
-  combine: (that) => (self) => self !== 0 ? self : that,
-  combineMany: (collection) =>
-    (self) => {
-      let ordering = self
-      if (ordering !== 0) {
-        return ordering
-      }
-      for (ordering of collection) {
-        if (ordering !== 0) {
-          return ordering
-        }
-      }
-      return ordering
-    }
-}
-
-/**
- * @category instances
- * @since 1.0.0
- */
-export const Monoid: monoid.Monoid<TotalOrdering> = monoid.fromAssociative(Associative, 0)
