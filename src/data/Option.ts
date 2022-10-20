2f2d5023ce7960789b833c82aeed40c3bb46cc17
gcanti <giulio.canti@gmail.com>
2022-10-20 05:49:01 +0200

    add type lambdas

--- src/data/Option.ts
+++ src/data/Option.ts
@@ -2,6 +2,8 @@
  * @since 1.0.0
  */
 
+import type { TypeLambda } from "@fp-ts/core/HKT"
+
 /**
  * @category models
  * @since 1.0.0
@@ -24,3 +26,11 @@ export interface Some<A> {
  * @since 1.0.0
  */
 export type Option<A> = None | Some<A>
+
+/**
+ * @category type lambdas
+ * @since 1.0.0
+ */
+export interface OptionTypeLambda extends TypeLambda {
+  readonly type: Option<this["Out"]>
+}
