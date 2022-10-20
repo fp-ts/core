51713b443092a961877827c44ef6ca9d84fc7b77
gcanti <giulio.canti@gmail.com>
2022-10-20 05:49:01 +0200

    add type lambdas

--- src/data/Either.ts
+++ src/data/Either.ts
@@ -2,6 +2,8 @@
  * @since 1.0.0
  */
 
+import type { TypeLambda } from "@fp-ts/core/HKT"
+
 /**
  * @category models
  * @since 1.0.0
@@ -25,3 +27,11 @@ export interface Right<A> {
  * @since 1.0.0
  */
 export type Either<E, A> = Left<E> | Right<A>
+
+/**
+ * @category type lambdas
+ * @since 1.0.0
+ */
+export interface EitherTypeLambda extends TypeLambda {
+  readonly type: Either<this["Out2"], this["Out"]>
+}
--- src/data/Predicate.ts
+++ src/data/Predicate.ts
@@ -2,10 +2,20 @@
  * @since 1.0.0
  */
 
+import type { TypeLambda } from "@fp-ts/core/HKT"
+
 /**
  * @category models
  * @since 1.0.0
  */
 export interface Predicate<A> {
   (a: A): boolean
+}
+
+/**
+ * @category type lambdas
+ * @since 1.0.0
+ */
+export interface PredicateTypeLambda extends TypeLambda {
+  readonly type: Predicate<this["In"]>
 }
