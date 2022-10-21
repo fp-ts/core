d94f453aef661d3bf8cb688f3e4828797b170f0f
gcanti <giulio.canti@gmail.com>
2022-10-21 13:08:26 +0200

    chore

--- src/typeclass/NonEmptyProduct.ts
+++ src/typeclass/NonEmptyProduct.ts
@@ -2,8 +2,6 @@
  * @since 1.0.0
  */
 import type { Kind, TypeClass, TypeLambda } from "@fp-ts/core/HKT"
-import { pipe } from "@fp-ts/core/internal/Function"
-import type { Covariant } from "@fp-ts/core/typeclass/Covariant"
 
 /**
  * @category type class
@@ -19,30 +17,4 @@ export interface NonEmptyProduct<F extends TypeLambda> extends TypeClass<F> {
   readonly productMany: <S, R, O, E, A>(
     collection: Iterable<Kind<F, S, R, O, E, A>>
   ) => (self: Kind<F, S, R, O, E, A>) => Kind<F, S, R, O, E, readonly [A, ...ReadonlyArray<A>]>
-}
-
-/**
- * @category constructors
- * @since 1.0.0
- */
-export const fromCovariant = <F extends TypeLambda>(
-  Covariant: Covariant<F>,
-  product: NonEmptyProduct<F>["product"]
-): NonEmptyProduct<F> => {
-  return {
-    product,
-    productMany: <S, R, O, E, A>(
-      collection: Iterable<Kind<F, S, R, O, E, A>>
-    ) =>
-      (self: Kind<F, S, R, O, E, A>) => {
-        let out: Kind<F, S, R, O, E, [A, ...Array<A>]> = pipe(
-          self,
-          Covariant.map(a => [a])
-        )
-        for (const fa of collection) {
-          out = pipe(out, product(fa), Covariant.map(([[head, ...tail], a]) => [head, ...tail, a]))
-        }
-        return out
-      }
-  }
 }
