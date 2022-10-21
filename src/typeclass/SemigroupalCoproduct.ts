02fdebe655885b84a364d59b2bae008bc6ac49b6
gcanti <giulio.canti@gmail.com>
2022-10-21 07:04:45 +0200

    SemigroupalCoproduct: add fromCoproduct

--- src/typeclass/SemigroupalCoproduct.ts
+++ src/typeclass/SemigroupalCoproduct.ts
@@ -2,6 +2,7 @@
  * @since 1.0.0
  */
 import type { Kind, TypeClass, TypeLambda } from "@fp-ts/core/HKT"
+import { pipe } from "@fp-ts/core/internal/Function"
 
 /**
  * @category type class
@@ -17,4 +18,24 @@ export interface SemigroupalCoproduct<F extends TypeLambda> extends TypeClass<F>
   readonly coproductMany: <S, R, O, E, A>(
     collection: Iterable<Kind<F, S, R, O, E, A>>
   ) => (self: Kind<F, S, R, O, E, A>) => Kind<F, S, R, O, E, A>
+}
+
+/**
+ * @category constructors
+ * @since 1.0.0
+ */
+export const fromCoproduct = <F extends TypeLambda>(
+  coproduct: SemigroupalCoproduct<F>["coproduct"]
+): SemigroupalCoproduct<F> => {
+  return {
+    coproduct,
+    coproductMany: collection =>
+      self => {
+        let out = self
+        for (const fa of collection) {
+          out = pipe(out, coproduct(fa))
+        }
+        return out
+      }
+  }
 }
