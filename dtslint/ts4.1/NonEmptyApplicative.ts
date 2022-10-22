03f418efdfaf5fd9e132df041d43a1b613592996
gcanti <giulio.canti@gmail.com>
2022-10-22 07:45:09 +0200

    FlatMap: add flatten

--- dtslint/ts4.1/NonEmptyApplicative.ts
+++ dtslint/ts4.1/NonEmptyApplicative.ts
@@ -1,28 +1,22 @@
-import * as nonEmptyApplicative from "@fp-ts/core/typeclass/NonEmptyApplicative"
+import * as _ from "@fp-ts/core/typeclass/NonEmptyApplicative"
 import type { TypeLambda } from "@fp-ts/core/HKT"
 
-export interface ReaderAsyncWriter<R, E, A> {
-  (r: R): () => Promise<[E, A]>
+interface RAW<R, E, A> {
+  (r: R): () => Promise<readonly [E, A]>
 }
 
-export interface ReaderAsyncWriterTypeLambda extends TypeLambda {
-  readonly type: ReaderAsyncWriter<this["In"], this["Out1"], this["Target"]>
+interface RAWTypeLambda extends TypeLambda {
+  readonly type: RAW<this["In"], this["Out1"], this["Target"]>
 }
 
-declare const fa: ReaderAsyncWriter<{ a: string }, "a", string>
-declare const fb: ReaderAsyncWriter<{ b: number }, "b", number>
-declare const fc: ReaderAsyncWriter<{ c: boolean }, "c", boolean>
+declare const fa: RAW<{ a: string }, "a", string>
+declare const fb: RAW<{ b: number }, "b", number>
+declare const fc: RAW<{ c: boolean }, "c", boolean>
 
-export declare const NonEmptyApplicative: nonEmptyApplicative.NonEmptyApplicative<ReaderAsyncWriterTypeLambda>
+declare const NonEmptyApplicative: _.NonEmptyApplicative<RAWTypeLambda>
 
-// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [string, number, boolean]>
-nonEmptyApplicative.lift3(NonEmptyApplicative)((a: string, b: number, c: boolean) => [a, b, c] as const)(fa, fb, fc)
+// $ExpectType RAW<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [string, number, boolean]>
+_.lift3(NonEmptyApplicative)((a: string, b: number, c: boolean) => [a, b, c] as const)(fa, fb, fc)
 
-// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [unknown, unknown, unknown]>
-nonEmptyApplicative.lift3(NonEmptyApplicative)(<A, B, C>(a: A, b: B, c: C): readonly [A, B, C] => [a, b, c])(fa, fb, fc)
-
-export const f: <R1, E1, A, R2, E2, B, R3, E3, C>(
-  fa: ReaderAsyncWriter<R1, E1, A>,
-  fb: ReaderAsyncWriter<R2, E2, B>,
-  fc: ReaderAsyncWriter<R3, E3, C>
-) => ReaderAsyncWriter<R1 & R2 & R3, E1 | E2 | E3, readonly [A, B, C]> = nonEmptyApplicative.lift3(NonEmptyApplicative)(<A, B, C>(a: A, b: B, c: C): readonly [A, B, C] => [a, b, c])
+// $ExpectType RAW<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [unknown, unknown, unknown]>
+_.lift3(NonEmptyApplicative)(<A, B, C>(a: A, b: B, c: C): readonly [A, B, C] => [a, b, c])(fa, fb, fc)
--- dtslint/ts4.1/Product.ts
+++ dtslint/ts4.1/Product.ts
@@ -1,22 +1,22 @@
-import * as product from "@fp-ts/core/typeclass/Product"
+import * as _ from "@fp-ts/core/typeclass/Product"
 import type { TypeLambda } from "@fp-ts/core/HKT"
 
-export interface ReaderAsyncWriter<R, E, A> {
-  (r: R): () => Promise<[E, A]>
+interface RAW<R, E, A> {
+  (r: R): () => Promise<readonly [E, A]>
 }
 
-export interface ReaderAsyncWriterTypeLambda extends TypeLambda {
-  readonly type: ReaderAsyncWriter<this["In"], this["Out1"], this["Target"]>
+interface RAWTypeLambda extends TypeLambda {
+  readonly type: RAW<this["In"], this["Out1"], this["Target"]>
 }
 
-declare const fa: ReaderAsyncWriter<{ a: string }, "a", string>
-declare const fb: ReaderAsyncWriter<{ b: number }, "b", number>
-declare const fc: ReaderAsyncWriter<{ c: boolean }, "c", boolean>
+declare const fa: RAW<{ a: string }, "a", string>
+declare const fb: RAW<{ b: number }, "b", number>
+declare const fc: RAW<{ c: boolean }, "c", boolean>
 
-export declare const Product: product.Product<ReaderAsyncWriterTypeLambda>
+export declare const Product: _.Product<RAWTypeLambda>
 
-// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [string, number, boolean]>
-product.tuple(Product)(fa, fb, fc)
+// $ExpectType RAW<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", readonly [string, number, boolean]>
+_.tuple(Product)(fa, fb, fc)
 
-// $ExpectType ReaderAsyncWriter<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", { readonly fa: string; readonly fb: number; readonly fc: boolean; }>
-product.struct(Product)({ fa, fb, fc })
+// $ExpectType RAW<{ a: string; } & { b: number; } & { c: boolean; }, "a" | "b" | "c", { readonly fa: string; readonly fb: number; readonly fc: boolean; }>
+_.struct(Product)({ fa, fb, fc })
