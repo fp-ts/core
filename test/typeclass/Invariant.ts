bfefd6c7c00ae77f414bef66b0e6b123e37d6fe4
gcanti <giulio.canti@gmail.com>
2022-10-23 07:34:41 +0200

    Invariant: add imapComposition

--- test/typeclass/Invariant.ts
+++ test/typeclass/Invariant.ts
@@ -1,10 +1,24 @@
 import { pipe } from "@fp-ts/core/internal/Function"
 import * as _ from "@fp-ts/core/typeclass/Invariant"
+import * as semigroup from "@fp-ts/core/typeclass/Semigroup"
 import * as O from "../test-data/Option"
 import * as P from "../test-data/Predicate"
+import * as string from "../test-data/string"
 import * as U from "../util"
 
 describe("Invariant", () => {
+  it("imapComposition", () => {
+    const imap = _.imapComposition(semigroup.Invariant, O.Invariant)
+    const S = pipe(O.getMonoid(string.Semigroup), imap(s => [s] as const, ([s]) => s))
+    U.deepStrictEqual(pipe(O.none, S.combine(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.none, S.combine(O.some(["b"]))), O.some(["b"] as const))
+    U.deepStrictEqual(pipe(O.some(["a"] as const), S.combine(O.none)), O.some(["a"] as const))
+    U.deepStrictEqual(
+      pipe(O.some(["a"] as const), S.combine(O.some(["b"]))),
+      O.some(["ab"] as const)
+    )
+  })
+
   describe("bindTo", () => {
     it("Covariant (Option)", () => {
       const bindTo = _.bindTo(O.Invariant)
