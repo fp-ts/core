bb5836384c35cd0ff9510ff54eb849e6dd03a388
gcanti <giulio.canti@gmail.com>
2022-10-23 10:13:02 +0200

    Coproduct: add getMonoid

---
+++ test/typeclass/Coproduct.ts
@@ -0,0 +1,18 @@
+import { pipe } from "@fp-ts/core/internal/Function"
+import * as _ from "@fp-ts/core/typeclass/Coproduct"
+import * as O from "../test-data/Option"
+import * as U from "../util"
+
+describe("Coproduct", () => {
+  it("getMonoid", () => {
+    const M = _.getMonoid(O.Alternative)<number>()
+    U.deepStrictEqual(pipe(O.none, M.combine(O.none)), O.none)
+    U.deepStrictEqual(pipe(O.some(1), M.combine(O.none)), O.some(1))
+    U.deepStrictEqual(pipe(O.none, M.combine(O.some(2))), O.some(2))
+    U.deepStrictEqual(pipe(O.some(1), M.combine(O.some(2))), O.some(1))
+
+    U.deepStrictEqual(pipe(M.empty, M.combine(O.none)), O.none)
+    U.deepStrictEqual(pipe(M.empty, M.combine(O.some(2))), O.some(2))
+    U.deepStrictEqual(pipe(O.some(1), M.combine(M.empty)), O.some(1))
+  })
+})
