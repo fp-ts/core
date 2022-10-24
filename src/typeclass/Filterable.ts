cd5e87afb0458f0839e6c58252a15f68c684c7c5
gcanti <giulio.canti@gmail.com>
2022-10-24 06:41:40 +0200

    remove Predicate from /data

--- src/typeclass/Filterable.ts
+++ src/typeclass/Filterable.ts
@@ -5,7 +5,6 @@
  */
 import type { Either } from "@fp-ts/core/data/Either"
 import type { Option } from "@fp-ts/core/data/Option"
-import type { Predicate } from "@fp-ts/core/data/Predicate"
 import type { Refinement } from "@fp-ts/core/data/Refinement"
 import type { Kind, TypeClass, TypeLambda } from "@fp-ts/core/HKT"
 import * as either from "@fp-ts/core/internal/Either"
@@ -48,11 +47,11 @@ export const filter: <F extends TypeLambda>(
     self: Kind<F, R, O, E, C>
   ) => Kind<F, R, O, E, B>
   <B extends A, A = B>(
-    predicate: Predicate<A>
+    predicate: (a: A) => boolean
   ): <R, O, E>(self: Kind<F, R, O, E, B>) => Kind<F, R, O, E, B>
 } = <F extends TypeLambda>(Filterable: Filterable<F>) =>
   <B extends A, A = B>(
-    predicate: Predicate<A>
+    predicate: (a: A) => boolean
   ): (<R, O, E>(self: Kind<F, R, O, E, B>) => Kind<F, R, O, E, B>) =>
     Filterable.filterMap(b => (predicate(b) ? option.some(b) : option.none))
 
@@ -79,13 +78,13 @@ export const partition: <F extends TypeLambda>(
   <C extends A, B extends A, A = C>(refinement: Refinement<A, B>): <R, O, E>(
     self: Kind<F, R, O, E, C>
   ) => readonly [Kind<F, R, O, E, C>, Kind<F, R, O, E, B>]
-  <B extends A, A = B>(predicate: Predicate<A>): <R, O, E>(
+  <B extends A, A = B>(predicate: (a: A) => boolean): <R, O, E>(
     self: Kind<F, R, O, E, B>
   ) => readonly [Kind<F, R, O, E, B>, Kind<F, R, O, E, B>]
 } = <F extends TypeLambda>(Filterable: Filterable<F>) => {
   const partitionMap_ = partitionMap(Filterable)
   return <B extends A, A = B>(
-    predicate: Predicate<A>
+    predicate: (a: A) => boolean
   ): (<R, O, E>(
     self: Kind<F, R, O, E, B>
   ) => readonly [Kind<F, R, O, E, B>, Kind<F, R, O, E, B>]) =>
