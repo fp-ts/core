4b74719e609fc42f7c726b7ad4b80467a073f1fd
gcanti <giulio.canti@gmail.com>
2022-10-27 06:56:05 +0200

    chore

--- src/typeclass/Product.ts
+++ src/typeclass/Product.ts
@@ -20,28 +20,28 @@ export interface Product<F extends TypeLambda> extends NonEmptyProduct<F>, Of<F>
  * @since 1.0.0
  */
 export const tuple = <F extends TypeLambda>(F: Product<F>) =>
-  <T extends ReadonlyArray<Kind<F, any, any, any, any>>>(...tuple: T): Kind<
+  <T extends ReadonlyArray<Kind<F, any, any, any, any>>>(...components: T): Kind<
     F,
     ([T[number]] extends [Kind<F, infer R, any, any, any>] ? R : never),
     ([T[number]] extends [Kind<F, any, infer O, any, any>] ? O : never),
     ([T[number]] extends [Kind<F, any, any, infer E, any>] ? E : never),
     Readonly<{ [I in keyof T]: [T[I]] extends [Kind<F, any, any, any, infer A>] ? A : never }>
-  > => F.productAll(tuple) as any
+  > => F.productAll(components) as any
 
 /**
  * @since 1.0.0
  */
 export const struct = <F extends TypeLambda>(F: Product<F>) =>
-  <R extends Record<string, Kind<F, any, any, any, any>>>(r: R): Kind<
+  <R extends Record<string, Kind<F, any, any, any, any>>>(fields: R): Kind<
     F,
     ([R[keyof R]] extends [Kind<F, infer R, any, any, any>] ? R : never),
     ([R[keyof R]] extends [Kind<F, any, infer O, any, any>] ? O : never),
     ([R[keyof R]] extends [Kind<F, any, any, infer E, any>] ? E : never),
     { readonly [K in keyof R]: [R[K]] extends [Kind<F, any, any, any, infer A>] ? A : never }
   > => {
-    const keys = Object.keys(r)
+    const keys = Object.keys(fields)
     return pipe(
-      F.productAll(keys.map(k => r[k])),
+      F.productAll(keys.map(k => fields[k])),
       F.imap(values => {
         const out: any = {}
         for (let i = 0; i < values.length; i++) {
