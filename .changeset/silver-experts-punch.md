15db2e24d62a4cffc2aa2f5e9f8445c3e6156168
gcanti <giulio.canti@gmail.com>
2023-01-15 15:56:39 +0100

    add Ordering module

---
+++ .changeset/silver-experts-punch.md
@@ -0,0 +1,5 @@
+---
+"@fp-ts/core": patch
+---
+
+add Ordering module
---
+++ test/Ordering.ts
@@ -0,0 +1,46 @@
+import { pipe } from "@fp-ts/core/Function"
+import * as _ from "@fp-ts/core/Ordering"
+import { deepStrictEqual } from "./util"
+
+describe("Ordering", () => {
+  it("match", () => {
+    const f = _.match(
+      () => "lt",
+      () => "eq",
+      () => "gt"
+    )
+    deepStrictEqual(f(-1), "lt")
+    deepStrictEqual(f(0), "eq")
+    deepStrictEqual(f(1), "gt")
+  })
+
+  it("reverse", () => {
+    deepStrictEqual(_.reverse(-1), 1)
+    deepStrictEqual(_.reverse(0), 0)
+    deepStrictEqual(_.reverse(1), -1)
+  })
+
+  it("Semigroup", () => {
+    deepStrictEqual(pipe(0, _.Semigroup.combine(0)), 0)
+    deepStrictEqual(pipe(0, _.Semigroup.combine(1)), 1)
+    deepStrictEqual(pipe(1, _.Semigroup.combine(-1)), 1)
+    deepStrictEqual(pipe(-1, _.Semigroup.combine(1)), -1)
+
+    deepStrictEqual(pipe(0, _.Semigroup.combineMany([])), 0)
+    deepStrictEqual(pipe(1, _.Semigroup.combineMany([])), 1)
+    deepStrictEqual(pipe(-1, _.Semigroup.combineMany([])), -1)
+    deepStrictEqual(pipe(0, _.Semigroup.combineMany([0, 0, 0])), 0)
+    deepStrictEqual(pipe(0, _.Semigroup.combineMany([0, 0, 1])), 1)
+    deepStrictEqual(pipe(1, _.Semigroup.combineMany([0, 0, -1])), 1)
+    deepStrictEqual(pipe(-1, _.Semigroup.combineMany([0, 0, 1])), -1)
+  })
+
+  it("Monoid", () => {
+    deepStrictEqual(pipe(_.Monoid.empty, _.Monoid.combine(0)), 0)
+    deepStrictEqual(pipe(_.Monoid.empty, _.Monoid.combine(1)), 1)
+    deepStrictEqual(pipe(_.Monoid.empty, _.Monoid.combine(-1)), -1)
+    deepStrictEqual(pipe(0, _.Monoid.combine(_.Monoid.empty)), 0)
+    deepStrictEqual(pipe(1, _.Monoid.combine(_.Monoid.empty)), 1)
+    deepStrictEqual(pipe(-1, _.Monoid.combine(_.Monoid.empty)), -1)
+  })
+})
