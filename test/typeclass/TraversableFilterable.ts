329c42a85e2ed82f5d28d1bb61e370e62ec427ce
gcanti <giulio.canti@gmail.com>
2023-01-16 15:26:12 +0100

    ReadonlyArray: add CovariantWithIndex, FilterableWithIndex instances

---
+++ test/typeclass/TraversableFilterable.ts
@@ -0,0 +1,36 @@
+import * as O from "@fp-ts/core/Option"
+import * as RA from "@fp-ts/core/ReadonlyArray"
+import * as _ from "@fp-ts/core/typeclass/TraversableFilterable"
+import * as U from "../util"
+
+describe("TraversableFilterable", () => {
+  it("traverseFilter", () => {
+    const traverseFilter = _.traverseFilter(
+      RA.TraversableFilterable
+    )(O.Applicative)
+    const f = traverseFilter((s: string) =>
+      s.length > 2 ? O.some(false) : s.length > 1 ? O.some(true) : O.none()
+    )
+    U.deepStrictEqual(f([]), O.some([]))
+    U.deepStrictEqual(f(["a"]), O.none())
+    U.deepStrictEqual(f(["a", "aa"]), O.none())
+    U.deepStrictEqual(f(["aa"]), O.some(["aa"]))
+    U.deepStrictEqual(f(["aaa"]), O.some([]))
+    U.deepStrictEqual(f(["aaa", "aa"]), O.some(["aa"]))
+  })
+
+  it("traversePartition", () => {
+    const traversePartition = _.traversePartition(
+      RA.TraversableFilterable
+    )(O.Applicative)
+    const f = traversePartition((s: string) =>
+      s.length > 2 ? O.some(false) : s.length > 1 ? O.some(true) : O.none()
+    )
+    U.deepStrictEqual(f([]), O.some([[], []] as const))
+    U.deepStrictEqual(f(["a"]), O.none())
+    U.deepStrictEqual(f(["a", "aa"]), O.none())
+    U.deepStrictEqual(f(["aa"]), O.some([[], ["aa"]] as const))
+    U.deepStrictEqual(f(["aaa"]), O.some([["aaa"], []] as const))
+    U.deepStrictEqual(f(["aaa", "aa"]), O.some([["aaa"], ["aa"]] as const))
+  })
+})
