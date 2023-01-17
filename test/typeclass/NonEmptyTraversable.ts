0c70d3e40bded81fa1e3b31d7ebd85c80db1ece2
gcanti <giulio.canti@gmail.com>
2023-01-17 18:31:23 +0100

    ReadonlyArray: remove useless instance

--- test/typeclass/NonEmptyTraversable.ts
+++ test/typeclass/NonEmptyTraversable.ts
@@ -1,15 +1,32 @@
 import { identity, pipe } from "@fp-ts/core/Function"
+import type { TypeLambda } from "@fp-ts/core/HKT"
 import * as O from "@fp-ts/core/Option"
 import * as RA from "@fp-ts/core/ReadonlyArray"
 import * as _ from "@fp-ts/core/test/limbo/NonEmptyTraversable"
+import * as covariant from "@fp-ts/core/typeclass/Covariant"
 import * as U from "../util"
 
-const NonEmptyTraversable: _.NonEmptyTraversable<RA.NonEmptyReadonlyArrayTypeLambda> = {
+const NonEmptyTraversable: _.NonEmptyTraversable<NonEmptyReadonlyArrayTypeLambda> = {
   // @ts-expect-error
   traverseNonEmpty: RA.traverseNonEmpty,
   // @ts-expect-error
   sequenceNonEmpty: F => self => pipe(self, RA.traverseNonEmpty(F)(identity))
 }
+
+/**
+ * @category type lambdas
+ * @since 1.0.0
+ */
+export interface NonEmptyReadonlyArrayTypeLambda extends TypeLambda {
+  readonly type: RA.NonEmptyReadonlyArray<this["Target"]>
+}
+
+/**
+ * @category instances
+ * @since 1.0.0
+ */
+export const NonEmptyCovariant: covariant.Covariant<NonEmptyReadonlyArrayTypeLambda> = covariant
+  .make(RA.mapNonEmpty)
 
 describe("NonEmptyTraversable", () => {
   it("traverseNonEmptyComposition", () => {
@@ -25,7 +42,7 @@ describe("NonEmptyTraversable", () => {
 
   it("traverseNonEmptyComposition", () => {
     const sequence = _.sequenceNonEmptyComposition(
-      { ...NonEmptyTraversable, ...RA.NonEmptyCovariant },
+      { ...NonEmptyTraversable, ...NonEmptyCovariant },
       NonEmptyTraversable
     )(O.SemiApplicative)
     U.deepStrictEqual(sequence([[O.some(1)]]), O.some([[1]] as const))
@@ -41,7 +58,7 @@ describe("NonEmptyTraversable", () => {
   })
 
   it("sequenceNonEmpty", () => {
-    const sequenceNonEmpty = _.sequenceNonEmpty<RA.NonEmptyReadonlyArrayTypeLambda>(
+    const sequenceNonEmpty = _.sequenceNonEmpty<NonEmptyReadonlyArrayTypeLambda>(
       NonEmptyTraversable.traverseNonEmpty
     )(O.SemiApplicative)
     U.deepStrictEqual(sequenceNonEmpty([O.none()]), O.none())
