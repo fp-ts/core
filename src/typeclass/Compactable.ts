a6e094e35df221921b9267f0865006d54524ffd3
gcanti <giulio.canti@gmail.com>
2023-01-17 08:37:32 +0100

    Compactable: remove readonly from separate

--- src/typeclass/Compactable.ts
+++ src/typeclass/Compactable.ts
@@ -38,7 +38,7 @@ export const separate = <F extends TypeLambda>(
 ) =>
   <R, O, E, A, B>(
     self: Kind<F, R, O, E, Either<A, B>>
-  ): readonly [Kind<F, R, O, E, A>, Kind<F, R, O, E, B>] => {
+  ): [Kind<F, R, O, E, A>, Kind<F, R, O, E, B>] => {
     return [
       pipe(self, F.map(either.getLeft), F.compact),
       pipe(self, F.map(either.getRight), F.compact)
