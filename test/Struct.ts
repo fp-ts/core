c1f7bd9b0544b2bbef3f0c8e83e1ad31a3fcf08e
gcanti <giulio.canti@gmail.com>
2023-01-21 10:16:46 +0100

    Order: add struct

--- test/Struct.ts
+++ test/Struct.ts
@@ -2,6 +2,15 @@ import { pipe } from "@fp-ts/core/Function"
 import * as S from "@fp-ts/core/Struct"
 
 describe.concurrent("Struct", () => {
+  it("exports", () => {
+    expect(S.getEquivalence).exists
+    expect(S.getOrder).exists
+    expect(S.getSemigroup).exists
+    expect(S.getMonoid).exists
+    expect(S.nonEmptyProduct).exists
+    expect(S.product).exists
+  })
+
   it("pick", () => {
     expect(pipe({ a: "a", b: 1, c: true }, S.pick("a", "b"))).toEqual({ a: "a", b: 1 })
   })
